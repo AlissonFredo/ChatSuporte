8caa17a5b093e560aa686efc8ff3175ec44908f8
alisson fredo <douglasalissonsf@gmail.com>
2021-04-21 16:26:53 -0300

    aula 02 - criei banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,9 @@
 import express from "express";
-
+import "./database";
+import {routes} from "./routes";
 
 const app = express();//create servidor
-
-app.get("/", (request, response) => {
-    return response.json({
-        message: "Ola NLW 05!",
-});
-})
-
-app.post("/", (request, response) => {
-    return response.json({
-        message: "Usuario salvo com sucesso!",
-    });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));//start servidor
