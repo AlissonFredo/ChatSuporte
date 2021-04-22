5ed28a40fb47246fdb7d6974d8c7d4435f1f6b44
alisson fredo <douglasalissonsf@gmail.com>
2021-04-22 17:09:59 -0300

    aula 03 - criei as entities user e menssages e desenvolvi os metodos de insert no DB

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,20 +1,21 @@
 import {Request, Response} from "express";
-import {getCustomRepository} from "typeorm";
-import {SettingsRepository} from "../repositories/SettingsRepository";
+import { SettingsService } from "../services/SettingsService";
 
 class SettingsController {
 
     async create(request: Request, response: Response) {
         const {chat, username} = request.body;
-        const settingsRepository = getCustomRepository(SettingsRepository);
-    
-        const settings = settingsRepository.create({
-            chat,
-            username
-        });
-    
-        await settingsRepository.save(settings);
-        return response.json(settings);
+        const settingsService = new SettingsService();
+        
+        try {
+            const settings = await settingsService.create({chat, username});
+            return response.json(settings); 
+        } catch (error) {
+            return response.status(400).json({
+                message: error.message,
+            })
+        }
+
     }
 }
 
---
+++ src/services/UsersService.ts
@@ -0,0 +1,36 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/user";
+import { UsersRepository } from "../repositories/UserRepository";
+
+class UsersService {
+    private usersRepository: Repository<User>;
+
+    constructor(){
+        this.usersRepository = getCustomRepository(UsersRepository);
+    }
+
+    async create(email: string) {
+
+        //verificar se usuario existe
+        const userExists = await this.usersRepository.findOne({
+            email,
+        })
+
+        //se existir, retornar user
+        if(userExists) {
+            return userExists;
+        }
+
+        //se não existir, salvar no DB
+        const user = this.usersRepository.create({
+            email,
+        });
+
+        await this.usersRepository.save(user);
+
+        return user;
+
+    }
+}
+
+export {UsersService};
